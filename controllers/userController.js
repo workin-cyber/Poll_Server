ddb6ef106c981bb73e29a86e04fe14729185a12c
yosefus <yosalsoni@gmail.com>
2021-12-30 22:55:02 +0200

    create the first functions

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,1 +1,50 @@
+// הרצת החיבור לדאטה בייס
+require('../db');
+
+// ייבוא המודל של המשתמשים
 const userModel = require('../models/user');
+
+// משתמש זמני
+const tempUser = {
+  idNumber: String(Math.random() * 999), //למניעת בעיה של שימוש חוזר באותו ערך יוניקי
+  phone: String(Math.random() * 999), //למניעת בעיה של שימוש חוזר באותו ערך יוניקי
+  firstName: 'meir',
+  lastName: 'cohen',
+  address: {
+    city: 'Tlv',
+    street: 'ravin 3',
+    apartment: '12',
+    zipCode: '499859',
+  },
+  email: `${String(Math.random() * 999)}@gmail.com`, //למניעת בעיה של שימוש חוזר באותו ערך יוניקי
+  education: [
+    {
+      name: 'some college',
+      years: 3,
+      eduType: 'college',
+    },
+  ],
+  maritalStatus: 'Single',
+  birthDate: Date.now(),
+  sectoralAffiliation: 'something',
+  gender: 'male',
+  permissions: 'viewer',
+  password: '1234',
+};
+
+// פונקציית יצירת משתמש יחיד
+const createOne = async (newUser) => {
+  const result = await userModel.create(newUser); // יצירה
+  console.log('new one', result); //  הדפסה לקונסול- לצורך בדיקה בלבד
+  return result;
+};
+
+// קריאת כל המשתמשים
+const readAll = async () => {
+  const result = await userModel.find(); //בקשה מהדאטה בייס לשלוח את כל התוצאות של המשתמשים
+  console.log('the users in the db', result); //  הדפסה לקונסול- לצורך בדיקה בלבד
+  return result;
+};
+
+readAll();
+// createOne(tempUser)
--- db.js
+++ db.js
@@ -1,13 +1,14 @@
-const
-    mongoose = require('mongoose'),
-    userName = 'kloiz',
-    pass = 'Kloiz1234',
-    connectionString = `mongodb+srv://${userName}:${pass}@cluster0.ho2ly.mongodb.net/pollsDB?retryWrites=true&w=majority`
-   
-mongoose.connect(connectionString)
-    .then(() => {
-        console.log('mongo connected successfuly!')
-    })
-    .catch(() => {
-        throw 'mongo not connected'
-    })
+const mongoose = require('mongoose'),
+  userName = 'kloiz',
+  pass = 'Kloiz1234',
+  connectionString = `mongodb+srv://${userName}:${pass}@cluster0.ho2ly.mongodb.net/pollsDB?retryWrites=true&w=majority`;
+
+mongoose
+  .connect(connectionString)
+  .then(() => {
+    console.log('mongo connected successfuly!');
+  })
+  .catch((err) => {
+    console.log(err);
+    throw 'mongo not connected';
+  });
